3e82f239a71906edda6c9d0351f800e2fc9f4592
LouisGusta <profissional.gustavoborges@outlook.com>
2022-02-08 15:00:57 -0300

    ajuste src

--- main/__init__.py
+++ main/__init__.py
@@ -7,22 +7,22 @@ from helper import generateJSON, verifyResult, saveBadResults, saveGoodResults
 
 if __name__ == '__main__':
     # running script params in json for create a diffusion
-    # while True: 
-    params = details.Infos()
-        # generateJSON(params)
-        # # SECTION PROCESSING DIFFUSION
-        # filePath = os.path.abspath(os.getcwd()) + '\..\simulador'
-        # fileName = 'diffusionVideo.exe'
-        # if os.path.exists(filePath +  '\\' + fileName):
-        #     process = subprocess.Popen(fileName, stdout=subprocess.PIPE, creationflags=0x08000000, shell=True, cwd=filePath)
-        #     process.communicate()
+    while True: 
+        params = details.Infos()
+        generateJSON(params)
+        # SECTION PROCESSING DIFFUSION
+        filePath = os.path.abspath(os.getcwd()) + '\..\simulador'
+        fileName = 'diffusionVideo.exe'
+        if os.path.exists(filePath +  '\\' + fileName):
+            process = subprocess.Popen(fileName, stdout=subprocess.PIPE, creationflags=0x08000000, shell=True, cwd=filePath)
+            process.communicate()
         
-        # # valid if result not is a full dark pixels or white pixels and save bad results
-        # if not verifyResult(params): 
-        #     saveBadResults(params)
-        # else: 
-        #     saveGoodResults(params)
-        #     break
+        # valid if result not is a full dark pixels or white pixels and save bad results
+        if not verifyResult(params): 
+            saveBadResults(params)
+        else: 
+            saveGoodResults(params)
+            break
     # SECTION RUN YOUTUBE UPLOADER
     upload_video.uploadVideo(params)
 
